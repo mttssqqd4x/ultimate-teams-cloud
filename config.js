97a2e9c6c58cee4375bd956a7e8c690ee92f1491
Frisbee-Teams <mttssqqd4x@privaterelay.appleid.com>
2026-06-18 20:29:31 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ULTIMATE_TEAMS_CONFIG = {
-  SUPABASE_URL: "https://fsdqkozqkshqwvmhq.supabase.co",
+  SUPABASE_URL: "https://fsdqkozqjshqooqwvmhq.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE"
 };
